0538e0c67960587244d518f60f33bb0a6b762152
shaunakmohotra-byte <shaunakmohotra@tagoreint.com>
2026-02-01 11:15:19 +0530

    Update wsgi.py

--- preorder_app/wsgi.py
+++ preorder_app/wsgi.py
@@ -1,8 +1,12 @@
-import app
-from app import create_app
+from preorder_app import create_app
+from werkzeug.middleware.proxy_fix import ProxyFix
 
-# Initialize the Flask application
 app = create_app()
+
+# This fixes the HTTPS redirect issue on Render
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 if __name__ == "__main__":
     app.run()
